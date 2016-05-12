dd04a77773ee593aeb31a0bbdabd2837b0237b50
elizlalala <eliz.marie.brown@gmail.com>
2016-05-12 15:23:28 -0700

    Update models to reflect proper associations with the User model

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,3 +1,3 @@
 class Response < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :questions
-  has_many :answers, through :questions
-  has_many :responses, through :questions
-  has_many :votes, through :questions
+  has_many :answers
+  has_many :responses
+  has_many :votes
 end
