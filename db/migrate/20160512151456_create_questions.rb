85baa8b163f11834ce54852dda61ec852d9c090f
elizlalala <eliz.marie.brown@gmail.com>
2016-05-12 15:45:27 -0700

    Add migrations

--- db/migrate/20160512151456_create_questions.rb
+++ db/migrate/20160512151456_create_questions.rb
@@ -1,4 +1,13 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
+  	create_table :questions do |t|
+  		t.belongs_to :user
+  		t.string :text
+  		t.integer :best_answer_id
+
+  		t.timestamps
+  	end
+
+  	add_index :questions, :best_answer_id
   end
 end
--- db/migrate/20160512151504_create_answers.rb
+++ db/migrate/20160512151504_create_answers.rb
@@ -1,4 +1,9 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
+  	  	create_table :answers do |t|
+  	  		t.belongs_to :user
+
+  	  		t.timestamps
+  	end
   end
 end
