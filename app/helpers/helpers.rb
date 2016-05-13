de57ec85019cbcc7b32f1a45212f4586d69eab38
Bill & Brian Mosley <pair+billdevcode.brianlmosley@devbootcamp.com>
2016-05-12 18:12:00 -0700

    Add session and helper methods

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,1 +1,25 @@
 use Rack::Session::Pool, :expire_after => 2592000
+
+helpers do
+
+  def current_user do
+    @user = User.find(params[:user_id])
+  end
+
+  def current_question do
+    @question = Question.find(params[:question_id])
+  end
+
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  def logout!
+    session[:user_id] = nil
+  end
+
+  def logged_in?
+    current_user
+  end
+
+end
