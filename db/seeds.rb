112795cd3912c925a6751e31ad4528168d204e48
kristallam <kristal.lam24@gmail.com>
2016-05-13 10:52:03 -0700

    Create seed files for questions and answers

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+#Create Users
+40.times do
+  User.create(
+             username: Faker::Internet.user_name,
+             password: Faker::Internet.password(8)
+             email: Faker::Internet.safe_email
+             )
+end
+
+#Create questions
+40.times do
+  user = User.all.sample
+  user.question << Question.create!(title: Faker::Hipster.sentence, text: Faker::Hipster.paragraph)
+end
+
+#Create answers
+40.times do
+  user = User.all.sample
+  user.answer << Answer.create!(text: Faker::Hipster.paragraph)
+end
